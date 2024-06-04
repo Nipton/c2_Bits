40b00fc716d992dd379a914ef682565700dbd000
Nipton <Nipton@NIPTON>
2024-06-04 18:28:28 +0400

    Добавьте файлы проекта.

---
+++ Bits.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c2_Bits
+{
+    interface IBits
+    {
+        public bool this[byte index]
+        {
+            get; set;
+        }
+    }
+
+    internal class Bits : IBits
+    {
+        public long Value { get; set; }
+
+        public Bits(byte value) 
+        {
+            Value = value;
+        }
+        public Bits(int value)
+        {
+            Value = value;
+        }
+        public Bits(long value)
+        {
+            Value = value;
+        }
+
+        public bool this[byte index]
+        {
+            get
+            {
+                return (Value & (1 << index)) != 0;
+            }
+            set
+            {
+                if(value)
+                {
+                    Value |= 1L << index;
+                }
+                else
+                {
+                    Value &= ~(1 << index);
+                }
+            }
+        }
+
+        public static implicit operator Bits(byte b)
+        {
+            return new Bits(b);
+        }
+        public static implicit operator Bits(long l)
+        {
+            return new Bits(l);
+        }
+        public static implicit operator Bits(int i)
+        {
+            return new Bits(i);
+        }
+
+        public override string ToString()
+        {
+            return $"{Value.ToString()}  =  {Convert.ToString(Value, 2)} ";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace c2_Bits
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Bits bits = new Bits(10);
+            Console.WriteLine(bits);
+            Console.WriteLine(bits[0]);
+            Console.WriteLine(bits[1]);
+            Console.WriteLine(bits[2]);
+            Console.WriteLine(bits[3]);
+            bits[2] = true;
+            bits[0] = false;
+            Console.WriteLine(bits);
+            Console.WriteLine(bits[0]);
+            Console.WriteLine(bits[1]);
+            Console.WriteLine(bits[2]);
+            Console.WriteLine(bits[3]);
+            Bits bitsLong = new Bits(6L);
+            Bits bitsByte = new Bits((byte)6);
+            Console.WriteLine(bitsLong);
+            Console.WriteLine(bitsByte);
+
+            long longValue = 600000L;
+            int intValue = 20;
+            byte byteValue = 10;
+
+            Bits bitsByte2 = byteValue;
+            Console.WriteLine(bitsByte2);
+            Bits bitsLong2 = longValue;
+            Console.WriteLine(bitsLong2);
+            Bits bitsInt = intValue;
+            Console.WriteLine(bitsInt);
+        }
+    }
+}
